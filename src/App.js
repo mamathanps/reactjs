1187372062bb1b6deab40945b5e138003ef99ce5
mamathanps <mamatha.k8050@gmail.com>
2021-01-23 13:20:10 +0530

    Redirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt} from "react-router-dom";
+import Route from "react-router-dom/Route";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const User = (params) => {
+  return ( <h1> Welcome to our User Page {params.username}</h1>)
+}
+
+class App extends Component {
+  state = {
+    logIn: false
+  }
+  loginHandle = () => {
+    this.setState(prevState => ({
+      logIn: !prevState.logIn
+    }))
+  }
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+      <ul>
+        <li>
+          <NavLink to="/" exact activeStyle={{ color: 'green'}}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" exact activeStyle={{ color: 'green'}}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/user/Padmavathi. K" exact activeStyle={{ color: 'green'}}>User Padma</NavLink>
+        </li>
+        <li>
+          <NavLink to="/user/Nagesh. K" exact activeStyle={{ color: 'green'}}>User Nagesh</NavLink>
+        </li>
+        <li>
+          <NavLink to="/user/Satish" exact activeStyle={{ color: 'green'}}>User Satish</NavLink>
+        </li>
+      </ul> 
+
+      <Prompt
+        when={!this.state.logIn}
+        message={(location)=>{
+          return location.pathname.startsWith("/user") ? 'Are you sure want to continue this page?' 
+          : true
+        }}
+      />
+
+      <input type="button" value={this.state.logIn ? 'logout':'login'} 
+             onClick={this.loginHandle.bind(this)}/>
+
+        <Route exact strict path="/" render={
+          () => {
+            return ( <h1> Welcome to our Home Page..!!! </h1> );
+          }
+        }/>
+
+        <Route exact strict path="/about" render={
+          () => {
+            return ( <h1> Welcome to our About Page..!!! </h1> );
+          }
+        }/>
+        <Route exact strict path="/user/:username" render={({match})=>(
+          this.state.logIn ? ( 
+          <User username={match.params.username}/> ) : ( <Redirect to="/" /> )
+        )}/> 
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
